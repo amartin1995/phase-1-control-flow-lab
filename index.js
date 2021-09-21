4c4d90a349e71c83d2f76fb8c348bcda66cd454b
Alex Martin <alex.pmartin1@gmail.com>
2021-09-21 00:20:53 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetValue){
+  if (feetValue <= 400){
+    return `This one is on me!`
+  }  else if (feetValue > 2000){
+    return `I will gladly take your thirty bucks.`
+  } else if (feetValue > 2500){
+    return `No can do.`
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityValue){
+  return cityValue === `NYC`?`Ok, sounds good.` : `No go.`
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipValue){
+  switch(tipValue){
+    case `generous`:
+      return `Thank you so much.`
+    case `not as generous`:
+      return `Thank you.`
+    default:
+      return `Bye.`
+  }
 }
